160a6fb17fc4ca020f12901d4e447b5f67cb6ceb
1derek1 <derekvhkwok@gmail.com>
2021-09-26 00:10:49 +0800

    import file extension format (png, jpg, jepg)

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import os
 from flask import Flask, jsonify, request, render_template, Response
+from werkzeug.utils import secure_filename
+
+upload_folder = '/path/to/the/uploads'
+extension = {'png', 'jpg', 'jepg'}
 
 app = Flask(__name__)
-
+app.config['upload_folder'] = upload_folder
 
 @app.route("/")
+
+#def allow_file(filename);
+    #return '.' in filename and \
+        #filename.rsplit('.', 1)[1].lower() in extension
+
+#def uploadfile();
+    #if file.filename == '':
+        #flash('No selected file')
+        #return redirect(request.url)
+
 def get():
     test = {
         #"name": "moj helloworld",
